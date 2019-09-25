b9c33cde03d38c033c91c7fd4cfbedcab3c91d4d
Chris Schnaufer <schnaufer@email.arizona.edu>
2019-09-25 16:29:30 -0700

    Merging changes to master (#3)
    * Fixed queue name
    
    * Some fixes
    
    * Adding CyVerse docker template
    
    * Updating for CyVerse DE support

--- configuration.py
+++ configuration.py
@@ -3,7 +3,7 @@
 """Contains extractor configuration information
 """
 
-# Setup the name of our extractor.
+# Setup the name of our extractor. Alphanumeric characters are allows as well as hyphens and underscores '-' and '_'
 EXTRACTOR_NAME = ""
 
 # Name of scientific method for this extractor. Leave commented out if it's unknown
@@ -21,13 +21,31 @@ AUTHOR_NAME = ""
 # The email of the author of the extractor
 AUTHOR_EMAIL = ""
 
-# Reposity URI
+# Reposity URI of where the source code lives
 REPOSITORY = ""
+
+# Citation author for the algorithm
+CITATION_AUTHOR = ""
+
+# Citation title for the algorithm
+CITATION_TITLE = ""
+
+# Citation year for the algorithm
+CITATION_YEAR = ""
 
 # Output variable identifiers. Use a comma separated list if more than one value is returned.
 # For example, "variable 1,variable 2" identifies the two variables returned by the extractor.
 # If only one name is specified, no comma's are used.
 # Note that variable names cannot have comma's in them: use a different separator instead. Also,
 # all white space is kept intact; don't add any extra whitespace since it may cause name comparisons
-# to fail
+# to fail.
 VARIABLE_NAMES = ""
+
+# Uncomment this variable to indicate the extractor is to never write to TERRA REF Geostreams
+#NEVER_WRITE_GEOSTREAMS = True
+
+#Uncomment this variable to indicate the extractor is to never write to BETYdb
+#NEVER_WRITE_BETYDB = True
+
+# Uncomment this variable to never write CSV files
+#NEVER_WRITE_CSV = True
